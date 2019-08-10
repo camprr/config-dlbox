04c498d18cd747ed05591e3640ead62e6b098b3e
Robert Campbell <robert.campbell@insights2data.com>
2019-08-10 16:23:24 +0200

    None, really

---
+++ create_folders.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export .env
+mkdir -p $ROOT/Movies
+mkdir -p $ROOT/Series
+mkdir -p $ROOT/Downloads/complete
+mkdir -p $ROOT/Downloads/incomplete
+mkdir -p $ROOT/Downloads/torrent-blackhole
+mkdir -p $ROOT/config/jackett
+mkdir -p $ROOT/config/radarr
+mkdir -p $ROOT/config/sonarr
+mkdir -p $ROOT/config/plex/db
+mkdir -p $ROOT/config/plex/transcode
